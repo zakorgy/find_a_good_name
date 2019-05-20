f93ba913e883326a39d05c451e6c46d7f772520a
Zakor Gyula <gyula.zakor@h-lab.eu>
2019-05-20 16:58:28 +0200

    Add new room sprites and rng to room pick

--- src/level/level.rs
+++ src/level/level.rs
@@ -100,11 +100,11 @@ impl LevelBuilder {
     }
 
     fn create_rooms(&mut self) {
-        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("res/sprites/room1.png");
         let mut rng = thread_rng();
         let mut new_room_index = 0;
         let mut new_pos = (0, 0);
         for _ in 1..self.number_of_rooms {
+            let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(format!("res/sprites/room{}.png", rng.gen_range(1, 4)));
             for _ in 0..400 {
                 new_room_index = rng.gen_range(0, self.possible_positions.len());
                 new_pos = self.possible_positions[new_room_index];
