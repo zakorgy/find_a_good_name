8e1700c6161008728ccec25df17ccdc1a61f8100
Zakor Gyula <gyula.zakor@h-lab.eu>
2019-07-14 22:49:08 +0200

    Fix screen demensions

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ mod graphics;
 mod input;
 mod level;
 
-const WIDTH: u32 = 200;
-const HEIGHT: u32 = WIDTH / 16 * 9;
-const SCALE: u32 = 6;
+const WIDTH: u32 = 300;
+const HEIGHT: u32 = WIDTH / 15 * 9;
+const SCALE: u32 = 8;
 
 fn main() {
     let mut game = game::Game::new(WIDTH, HEIGHT, SCALE);
